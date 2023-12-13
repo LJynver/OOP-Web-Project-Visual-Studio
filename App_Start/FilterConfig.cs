461d103af46c5a38e0e240a7d7d927b22d2c0dcd
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2023-12-13 16:17:48 +0800

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OOP_Web_Project_Jover
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OOP_Web_Project_Jover.MvcApplication" Language="C#" %>
